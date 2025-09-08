19b8a17e359a0057e92195ce0fb7c248ce034669
deepanshu moorjani <89074398+deepanshu202002@users.noreply.github.com>
2025-09-09 02:02:08 +0530

    Update s3.tf

--- infra/terraform/s3.tf
+++ infra/terraform/s3.tf
@@ -1,4 +1,17 @@
 resource "aws_s3_bucket" "frontend_bucket" {
   bucket = "revuhub-frontend"
-  acl    = "public-read"
+  force_destroy = true
+
+  tags = {
+    Name = "revuhub-frontend"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "frontend_block" {
+  bucket = aws_s3_bucket.frontend_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
