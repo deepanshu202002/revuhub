b585446ae710223d788c499dd69d74e39d9b3c8b
deepanshu202002 <deepanshu.moorjani20@gmail.com>
2025-09-11 14:47:10 +0530

    making backend with cloudfront

--- infra/terraform/cloudfront.tf
+++ infra/terraform/cloudfront.tf
@@ -1,37 +1,15 @@
 resource "aws_cloudfront_distribution" "frontend_distribution" {
+  # S3 origin
   origin {
     domain_name = aws_s3_bucket.frontend_bucket.bucket_regional_domain_name
     origin_id   = "S3-revuhub"
   }
 
-  default_cache_behavior {
-    allowed_methods        = ["GET", "HEAD", "OPTIONS"]
-    cached_methods         = ["GET", "HEAD"]
-    target_origin_id       = "S3-revuhub"
-    viewer_protocol_policy = "redirect-to-https"
-
-    forwarded_values {
-      query_string = false
-      cookies { forward = "none" }
-    }
-
-    min_ttl     = 0
-    default_ttl = 3600
-    max_ttl     = 86400
-  }
-
-  restrictions {
-    geo_restriction { restriction_type = "none" }
-  }
-resource "aws_cloudfront_distribution" "frontend_distribution" {
+  # ALB origin for backend
   origin {
-    domain_name = aws_s3_bucket.frontend_bucket.bucket_regional_domain_name
-    origin_id   = "S3-revuhub"
-  }
-
-  origin {
-    domain_name = "revuhub-alb-122930133.ap-south-1.elb.amazonaws.com" # Your ALB DNS
+    domain_name = "revuhub-alb-122930133.ap-south-1.elb.amazonaws.com"
     origin_id   = "ALB-revuhub"
+
     custom_origin_config {
       http_port              = 80
       https_port             = 443
@@ -40,6 +18,7 @@ resource "aws_cloudfront_distribution" "frontend_distribution" {
     }
   }
 
+  # Default behavior: S3 frontend
   default_cache_behavior {
     allowed_methods        = ["GET", "HEAD", "OPTIONS"]
     cached_methods         = ["GET", "HEAD"]
@@ -56,6 +35,7 @@ resource "aws_cloudfront_distribution" "frontend_distribution" {
     max_ttl     = 86400
   }
 
+  # API behavior: ALB backend
   ordered_cache_behavior {
     path_pattern           = "/api/*"
     target_origin_id       = "ALB-revuhub"
@@ -77,15 +57,6 @@ resource "aws_cloudfront_distribution" "frontend_distribution" {
   restrictions {
     geo_restriction { restriction_type = "none" }
   }
-
-  viewer_certificate {
-    cloudfront_default_certificate = true
-  }
-
-  enabled             = true
-  is_ipv6_enabled     = true
-  default_root_object = "index.html"
-}
 
   viewer_certificate {
     cloudfront_default_certificate = true
