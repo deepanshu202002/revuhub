3c9bc5451d05217f09db0d5e5cb5819e15764c74
deepanshu202002 <deepanshu.moorjani20@gmail.com>
2025-09-11 12:44:26 +0530

    fixing alb subnet errors 2

--- infra/terraform/networking.tf
+++ infra/terraform/networking.tf
@@ -9,7 +9,7 @@ resource "aws_vpc" "revuhub_vpc" {
 # Public Subnet 1
 resource "aws_subnet" "public_1" {
   vpc_id                  = aws_vpc.revuhub_vpc.id
-  cidr_block              = "10.0.1.0/24"
+  cidr_block              = "10.0.11.0/24"
   map_public_ip_on_launch = true
   availability_zone       = data.aws_availability_zones.available.names[0]
   tags                    = { Name = "revuhub-public-subnet-1" }
@@ -18,7 +18,7 @@ resource "aws_subnet" "public_1" {
 # Public Subnet 2
 resource "aws_subnet" "public_2" {
   vpc_id                  = aws_vpc.revuhub_vpc.id
-  cidr_block              = "10.0.2.0/24"
+  cidr_block              = "10.0.12.0/24"
   map_public_ip_on_launch = true
   availability_zone       = data.aws_availability_zones.available.names[1]
   tags                    = { Name = "revuhub-public-subnet-2" }
